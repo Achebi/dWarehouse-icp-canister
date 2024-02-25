417d68e180f66d3fc32ed4e9236699888714f28a
Achebi <60100857+Achebi@users.noreply.github.com>
2024-02-25 21:04:15 +0300

    Update index.ts
    Updated the backend code to current version of azle

--- index.ts
+++ index.ts
@@ -1,202 +1,218 @@
-// cannister code goes here
-import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt, bool } from 'azle';
-import { v4 as uuidv4 } from 'uuid';
-
-/**
- * This type represents a warehouse real estate property that can be listed online for advertisement 
- */
-
+import { Canister, query, text, Record, Principal, bool, StableBTreeMap, Vec, Result,
+    Variant, ic, Ok, Err, Some, None, update } from 'azle';
+import { v4 as uuidv4 } from "uuid";
 
 /**
  * Define warehouse, user and client's data types
  */
-type Warehouse = Record<{
-    id: string;
-    name: string;
-    location: string;
-    maxColdRoomCapacity: nat64;
-    maxDryRoomCapacity: nat64;
-    listedOnline: bool;
-}>
+   const Warehouse = Record({
+    warehouseId: text,
+    userId: Principal,
+    warehouseName: text,
+    location: text,
+    maxColdRoomCapacity: text,
+    maxDryRoomCapacity: text,
+    listedOnline: bool,
+});
 
-type WarehousePayload = Record<{
-    name: string;
-    location: string;
-    maxColdRoomCapacity: nat64;
-    maxDryRoomCapacity: nat64;
-}>
+const WarehousePayload = Record({
+    warehouseName: text,
+    location: text,
+    maxColdRoomCapacity: text,
+    maxDryRoomCapacity: text,
+});
 
-type User = Record<{
-    id: string;
-    name: string;
-    userTelNo: string;
-    userEmail: string;
+const User =Record({
+    userId: Principal,
+    userName: text,
+    userTelNo: text,
+    userEmail: text,
+});
 
-}>
+const UserPayload =Record({
+    userName: text,
+    userTelNo: text,
+    userEmail: text,
+});
 
-type Client = Record<{
-    id: string;
-    name: string;
-    bookingDate: string;
-    bookedCapacity: nat64;
-}>
+const Client = Record({
+    warehouseId: text,
+    clientId: text,
+    clientName: text,
+    startDate: text,
+    endDate: text,
+    coldRoomSpaceSizeBooked: text,
+    dryRoomSpaceSizeBooked: text,
+});
+
+const ClientPayload = Record({
+    warehouseId: text,
+    clientName: text,
+    startDate: text,
+    endDate: text,
+    coldRoomSpaceSizeBooked: text,
+    dryRoomSpaceSizeBooked: text,
+});
+
+const Message = Variant({
+    NotFound: text,
+    InvalidPayload: text,
+});
 
 
 /**
  * Define storage for warehouses, users, and clients details
  */
-const warehouseStorage = new StableBTreeMap<string, Warehouse>(0, 44, 1024);
-const userStorage = new StableBTreeMap<string, User>(0, 44, 1024);
-const clientStorage = new StableBTreeMap<string, Client>(0, 44, 1024);
+const warehouseStorage = StableBTreeMap(1, text, Warehouse);
+const userStorage = StableBTreeMap(0, Principal, User);
+const clientStorage = StableBTreeMap(2, text, Client);
+
+var warehouseCounterId: number = 0;
+
+export default Canister({
+    /**
+     * Define functions for getting ALL warehouses, ALL users, and ALL clients
+     */
+    getWarehouses: query([], Vec(Warehouse), () => {
+        return warehouseStorage.values();
+    }),
+
+    getUsers: query([], Vec(User), () => {
+        return userStorage.values();
+    }),
+
+    getClients: query([], Vec(Client), () => {
+        return clientStorage.values();
+    }),
 
 
-/**
- * Define functions for getting ALL warehouses, users, and clients
- */
-$query;
-export function getWarehouse(id: string): Result<Vec<Warehouse>, string> {
-    return match(warehouseStorage.get(id), {
-        Some: (warehouse) => Result.Ok<Vec<Warehouse>, string>(warehouse),
-        None: () => Result.Err<Vec<Warehouse>, string>(`a warehouse with id=${id} not found`),
-    });
-}
-
-$query;
-export function getUser(id: string): Result<Vec<User>, string> {
-    return match(userStorage.get(id), {
-        Some: (user) => Result.Ok<Vec<User>, string>(user),
-        None: () => Result.Err<Vec<User>, string>(`a user with id=${id} not found`),
-    });
-}
-
-$query;
-export function getClient(id: string): Result<Vec<Client>, string> {
-    return match(clientStorage.get(id), {
-        Some: (client) => Result.Ok<Vec<Client>, string>(client),
-        None: () => Result.Err<Vec<Client>, string>(`a client with id=${id} not found`),
-    });
-}
-
-
-/**
- * Define functions for getting a SINGLE warehouse, user, and client
- */
-$query;
-export function getWarehouse(id: string): Result<Vec<Warehouse, string>{
-    return match(warehouseStorage.get(id), {
-        Some: (warehouse) => Result.Ok(<Warehouse, String>(warehouse)),
-        None: () => Result.Err(<Warehouse, String>('a warehouse with id=${id} not found')),
-    });
-
-$query;
-export function getUser(id: string): Result<Vec<User, string>{
-    return match(userStorage.get(id), {
-        Some: (user => Result.Ok(<Warehouse, String>(user)),
-        None: () => Result.Err(<User, String>('a user with id=${id} not found')),
-    });
-
-$query;
-export function getClient(id: string): Result<Vec<Client, string>{
-    return match(clientStorage.get(id), {
-        Some: (client) => Result.Ok(<Warehouse, String>(client)),
-        None: () => Result.Err(<Client, String>('a client with id=${id} not found')),
-    });
-
-
-/**
- * Define functions for adding a warehouse, a user, and a client
- */
-$update;
-export function addWarehouse(payload: WarehousePayload): Result<Warehouse, string> {
-    const warehouse: Warehouse = { id: uuidv4(), listedOnline: true, ...payload };
-    warehouseStorage.insert(warehouse.id, warehouse);
-    return Result.Ok<Warehouse, string>(warehouse);
-}
-
-$update;
-export function addUser(name: string, userTelNo: string, userEmail: string): Result<User, string> {
-    const user: User = { id: uuidv4(), name: name, userTelNo: userTelNo, userEmail: userEmail };
-    userStorage.insert(user.id, user);
-    return Result.Ok<User, string>(user);
-}
-
-
-$update;
-
-export function addClient(name: string, bookingDate: string, bookedCapacity: nat64): Result<Client, string> {
-    const client: Client = { id: uuidv4(), name: name, bookingDate: bookingDate, bookedCapacity: bookedCapacity };
-    clientStorage.insert(client.id, client);
-    return Result.Ok<Client, string>(client);
-}
-
-/**
- * Define functions for updating an existing warehouse, user, and client
- */
-$update;
-export function updateWarehouse(id: String, payload: WarehousePayload): Result<Warehouse, String>{
-    return match(warehouseStorage.get(id), {
-        Some: (warehouse) => {
-            const updatedWarehouse : Warehouse ={...warehouse, ...payload};
-            warehouseStorage.insert(warehouse.id, updatedWarehouse);
-            return Result.Ok<Warehouse, String>(updatedWarehouse);
-        },
-        None: () => Result.Err<Warehouse, String>(`Couldn't update a warehouse with id = ${id}. Warehouse not found`)
+    /**
+    * Define functions for getting a SINGLE warehouse, a SINGLE user, and a SINGLE client
+    */
+    getWarehouse: query([text], Result(Warehouse, Message), (id) => {
+        const warehouseOpt = warehouseStorage.get(id);
+        if ("None" in warehouseOpt) {
+            return Err({ NotFound: `warehouse with id=${id} not found`});
         }
+        return Ok(warehouseOpt.Some);
+   }),
+
+   getUser: query([text], Result(User, Message), (id) => {
+    const userOpt = userStorage.get(id);
+    if ("None" in userOpt) {
+        return Err({ NotFound: `userwith id=${id} not found`});
     }
+    return Ok(userOpt.Some);
+    }),
+
+    getClient: query([text], Result(Client, Message), (id) => {
+        const clientOpt = clientStorage.get(id);
+        if ("None" in clientOpt) {
+            return Err({ NotFound: `client with id=${id} not found`});
+        }
+        return Ok(clientOpt.Some);
+   }),
+
+    
+   /**
+    * Define functions for adding a warehouse, a user, and a client
+    */
+    addWarehouse: update([WarehousePayload], Result(Warehouse, Message), (payload) => {
+        if (Object.keys(payload).length === 0) {
+            return Err({InvalidPayload: "invalid warehouse payload"});
+        }
+        const _warehouseId: text = warehouseCounterId.toString();
+        warehouseCounterId += 1;
+
+        const warehouse = {warehouseId: _warehouseId, userId: ic.caller(), listedOnline: true, ...payload };
+        warehouseStorage.insert(warehouse.warehouseId, warehouse);
+        return Ok(warehouse);
+    }),
+
+    addUser: update([UserPayload], Result(User, Message), (payload) => {
+        if (Object.keys(payload).length === 0) {
+            return Err({InvalidPayload: "invalid user payload"});
+        }
+        const user = {userId: ic.caller(), ...payload };
+        userStorage.insert(user.userId, user);
+        return Ok(user);
+    }),
+    
+    addClient: update([ClientPayload], Result(Client, Message), (payload) => {
+        if (Object.keys(payload).length === 0) {
+            return Err({InvalidPayload: "invalid client payload"});
+        }
+        const client = {clientId: uuidv4(), ...payload };
+        clientStorage.insert(client.warehouseId, client);
+        return Ok(client);
+    }),
 
 
-$update;
-export function updateUser(id: String, name: String, userTelNo: String, userEmail: String): Result<User, String>{
-    return match(userStorage.get(id), {
-        Some: (user) => {
-            const updatedUser: User ={...user, name: String, userTelNo: String, userEmail: String};
-            userStorage.insert(user.id, updatedUser);
-            return Result.Ok<User, String>(updatedUser);
-        },
-        None: () => Result.Err<User, String>(`Couldn't update a user with id = ${id}. User not found`);
-    }
+    /**
+    * Define functions for updating an existing warehouse, user, and client
+    */
+    updateWarehouse: update([Warehouse], Result(Warehouse, Message), (payload) => {
+        const warehouseOpt = warehouseStorage.get(payload.warehouseId);
+        if ("None" in warehouseOpt) {
+            return Err({ NotFound: `cannot update the warehouse: warehouse with id=${payload.warehouseId} not found`});
+        }
+        const updatedWarehouse = {...payload}
+        warehouseStorage.insert(warehouseOpt.Some.warehouseId, updatedWarehouse);
+        return Ok(updatedWarehouse);
+    }),
 
-$update;
-export function updateClient(id: String, name: string, bookingDate: string, bookedCapacity: nat64 ): Result<Client, String>{
-    return match(clientStorage.get(id), {
-        Some: (client) => {
-            const updatedClient : Client ={...client, ...payload};
-            clientStorage.insert(client.id, updatedClient);
-            return Result.Ok<Client, String>(updatedClient);
-        },
-        None: () => Result.Err<Client, String>(`Couldn't update a client with id = ${id}. Client not found`);
-    }
+    updateUser: update([User], Result(User, Message), (payload) => {
+        const userOpt = userStorage.get(payload.userId);
+        if ("None" in userOpt) {
+            return Err({ NotFound: `cannot update the user: user with id=${payload.userId} not found`});
+        }
+        const updatedUser = {...payload}
+        warehouseStorage.insert(userOpt.Some.userId, updatedUser);
+        return Ok(updatedUser);
+    }),
+  
+    updateClient: update([Client], Result(Client, Message), (payload) => {
+        const clientOpt = clientStorage.get(payload.clientId);
+        if ("None" in clientOpt) {
+            return Err({NotFound: `cannot update the user: user with id=${payload.clientId} not found`});
+        }
+        const updatedClient = {...payload}
+        warehouseStorage.insert(clientOpt.Some.clientId, updatedClient);
+        return Ok(updatedClient);
+    }),
 
-/*
- * Define functions for deleting an existing warehouse, user, and client
- */
-$update;
-export function deleteWarehouse(id: String): Result<Warehouse, String>{
-    return match(warehouseStorage.remove(id), {
-        Some: (deletedWarehouse) => Result.Ok<Warehouse, String>(deletedWarehouse);
-        None: () => Result.Err<Warehouse, String>(`Couldn't delete a warehouse with id = ${id}. Warehouse not found`);
-    });
-     
-    }
 
-$update;
-export function deleteUser(id: String): Result<User, String>{
-    return match(userStorage.remove(id), {
-        Some: (deletedUser) => Result.Ok<User, String>(deletedUser);
-        None: () => Result.Err<User, String>(`Couldn't delete a user with id = ${id}. User not found`);
-    });
-        
-    }
+    /*
+    * Define functions for deleting an existing warehouse, user, and client
+    */
+    deleteWarehouse: update([text], Result(text, Message), (warehouseId) => {
+        const deletedWarehouseOpt = warehouseStorage.remove(warehouseId);
+        if ("None" in deletedWarehouseOpt) {
+            return Err({NotFound: `cannot delete the warehouse: warehouse with id=${warehouseId} not found`});
+        }
+        return Ok(deletedWarehouseOpt.Some.warehousId);
+    }),
 
-$update;
-export function deleteClient(id: String): Result<Client, String>{
-    return match(clientStorage.remove(id), {
-        Some: (deletedClient) => Result.Ok<Client, String>(deletedClient);
-        None: () => Result.Err<Client, String>(`Couldn't delete a client with id = ${id}. Client not found`);
-    });
-        
-    }
-// a workaround to make uuid package work with Azle
+    deleteUser: update([Principal], Result(text, Message), (userId) => {
+        const deletedUserOpt = userStorage.remove(userId);
+        if ("None" in deletedUserOpt) {
+            return Err({NotFound: `cannot delete the user: user with id=${userId} not found`});
+        }
+        return Ok(deletedUserOpt.Some.userId);
+    }),
+
+    deleteClient: update([text], Result(text, Message), (clientId) => {
+        const deletedClientOpt = clientStorage.remove(clientId);
+        if ("None" in deletedClientOpt) {
+            return Err({NotFound: `cannot delete the client: client with id=${clientId} not found`});
+        }
+        return Ok(deletedClientOpt.Some.clientId);
+    }),
+    
+
+})
+
+// a work around to make uuid package work with Azle
 globalThis.crypto = {
     //@ts-ignore
     getRandomValues: () => {
@@ -208,6 +224,4 @@ globalThis.crypto = {
 
         return array
     }
-}
-
 }
